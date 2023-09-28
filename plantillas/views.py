c96965fce1094b13ef68eaa091758bd3b29f210c
chrisardo <chrisardorolo02@gmail.com>
2023-09-28 17:08:37 -0500

    first commit

---
+++ plantillas/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def simple(request):
+    return render(request, "simple.html", {"name": "Mundo"})
+
+def dinamico(request, name):
+    categories = ["Programación", "Tecnología", "Ciencia"]
+    context = {"name": name, "categories": categories}
+    return render(request,"dinamico.html", context) 
+
+def estaticos(request):
+    return render(request, "estaticos.html") 
